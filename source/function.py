a3e86db554ed600fa6aae92296b8cf0b16f6b57b
raffryrizqullah <raffryrizqullah@gmail.com>
2025-01-21 22:20:32 +0700

    style:update formatting

--- source/function.py
+++ source/function.py
@@ -8,7 +8,7 @@ def read_file(data):
     documents = loader.load()
     return documents
 
-# Function to split extracted data into manageable text chunks
+
 def text_splitter(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
     text_chunks = text_splitter.split_documents(extracted_data)
